e1e57bcabb9685a38669d0b55f4db98c80dc97eb
oisdk <mail@doisinkidney.com>
2020-06-17 01:05:37 +0100

    proper paths

--- scripts/agda-from-toplevel.sh
+++ scripts/agda-from-toplevel.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 withcode=$1
 nocode=${withcode#"agda/"}
 cd agda || exit
--- scripts/check-all.sh
+++ scripts/check-all.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ( cd agda
 echo '{-# OPTIONS --cubical --safe #-}
 ' > Everything
